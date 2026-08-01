d48397361f741d3ed347df57183e7c93d0337132
Federico Tafur <fedetafur@vt.edu>
2026-08-01 01:51:59 -0500

    feat: add MicroclimateDetailPage with polling live results

--- web/src/app/router.tsx
+++ web/src/app/router.tsx
@@ -10,6 +10,7 @@ import UsersListPage from '../features/org-structure/pages/UsersListPage'
 import SystemSettingsPage from '../features/org-structure/pages/SystemSettingsPage'
 import DemographicFieldsPage from '../features/org-structure/pages/DemographicFieldsPage'
 import MicroclimatesListPage from '../features/microclimates/pages/MicroclimatesListPage'
+import MicroclimateDetailPage from '../features/microclimates/pages/MicroclimateDetailPage'
 import { getToken } from '../auth/token'
 import { decodeJwtPayload } from '../auth/jwt'
 import { resolveInitialRoute } from './resolveInitialRoute'
@@ -44,6 +45,7 @@ export const router = createBrowserRouter([
               { path: '/admin/companies/:companyId/demographic-fields', element: <DemographicFieldsPage /> },
               { path: '/admin/system-settings', element: <SystemSettingsPage /> },
               { path: '/microclimates', element: <MicroclimatesListPage /> },
+              { path: '/microclimates/:id', element: <MicroclimateDetailPage /> },
             ],
           },
         ],
